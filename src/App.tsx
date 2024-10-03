26826411dfee214d089d7bc5f41276ef22446dd9
Azizbek Muminov <azizbekphd@gmail.com>
2024-10-03 12:16:58 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import { useState } from 'react'
+import { useContext, useEffect, useMemo, useState } from 'react'
 import './App.css'
 import { Scene } from './components'
+import { MultipleContextProvider } from './utils'
+import { FocusContext, TrajectoriesContext, XRContext } from './contexts'
+import { createXRStore } from '@react-three/xr'
+import { Focus, Trajectory, TrajectoryUtils } from './types'
+import TimeControlsContext from './contexts/TimeControllerContext'
+
 
 function App() {
-    const [vr, setVr] = useState(false)
+    const xrStore = createXRStore();
+    const memoizedXrStore = useMemo(() => xrStore, [xrStore])
+    const [trajectories, setTrajectories] = useState<Trajectory[]>([])
+    const [timeControls, setTimeControls] = useState<TimeControlsState>({
+        time: new Date().getTime(),
+        deltaTime: 86400000
+    })
+
+    useEffect(() => {
+        TrajectoryUtils.load("data/objects.json").then(setTrajectories)
+    }, [])
 
     return (<>
-        <button onClick={() => {setVr(true)}}>Enter VR</button>
-        <Scene objects={[1]} vr={vr} />
+        <MultipleContextProvider contexts={[
+            {
+                context: TrajectoriesContext,
+                value: trajectories
+            },
+            {
+                context: TimeControlsContext,
+                value: timeControls
+            },
+            {context: FocusContext, value: new Focus()},
+            {context: XRContext, value: memoizedXrStore}
+        ]}>
+            <p>{new Date(timeControls.time).toISOString()}</p>
+            <input style={{width: "100%"}} type="range" min={timeControls.time - 31536000} max={timeControls.time + 31536000} step={1000} value={timeControls.time} onChange={(e) => {
+                setTimeControls({
+                    ...timeControls,
+                    time: parseInt(e.target.value)
+                })
+            }} />
+            <Scene />
+        </MultipleContextProvider>
     </>)
 }
 
